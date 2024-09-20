78595f6a4463ed46797d676b89ff3ea911a6f222
Slav Pshenov <spshenov@stbc-i1.nikhef.nl>
2024-09-20 16:46:04 +0200

    adding ignore file types

--- training.py
+++ training.py
@@ -3,5 +3,6 @@ from data_processing.dataset import load_trackml_data
 if __name__ == '__main__':
     DATA_PATH = '/data/atlas/users/spshenov/trackml_10to50tracks_40kevents.csv'
     MAX_NUM_HITS = 100
-    hits_data, track_params_data, track_classes_data = load_trackml_data(data_path=DATA_PATH, max_num_hits=MAX_NUM_HITS)
+    hits_data, track_params_data, track_classes_data = load_trackml_data(data=DATA_PATH, max_num_hits=MAX_NUM_HITS)
+    print(hits_data)
     print('finished loading the trackML data')
