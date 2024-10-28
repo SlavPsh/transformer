9929be6d072f70217bf9dce26934b63c06cbc0df
Slav Pshenov <spshenov@stbc-i1.nikhef.nl>
2024-10-28 20:02:25 +0100

    small change in training

--- training.py
+++ training.py
@@ -3,8 +3,6 @@ import torch
 import torch.nn as nn
 import numpy as np
 
-from hdbscan import HDBSCAN
-
 # Import supporting tools
 from utils.io_utils import load_config, setup_logging, unique_output_dir, copy_config_to_output, get_file_path
 from utils.wandb_utils import WandbLogger
@@ -130,23 +128,6 @@ def evaluate(model, validation_loader, loss_fn):
             losses += loss.item()
             
     return losses / len(validation_loader)
-
-def clustering(pred_params, min_cl_size, min_samples):
-    '''
-    Function to perform HDBSCAN on the predicted track parameters, with specified
-    HDBSCAN hyperparameters. Returns the associated cluster IDs.
-    '''
-    clustering_algorithm = HDBSCAN(min_cluster_size=min_cl_size, min_samples=min_samples)
-    cluster_labels = []
-    for _, event_prediction in enumerate(pred_params):
-        regressed_params = np.array(event_prediction.tolist())
-        event_cluster_labels = clustering_algorithm.fit_predict(regressed_params)
-        cluster_labels.append(event_cluster_labels)
-
-    cluster_labels = [torch.from_numpy(cl_lbl).int() for cl_lbl in cluster_labels]
-    return cluster_labels
-    
-
 
 def custom_mse_loss(predictions, targets, weights):
 
